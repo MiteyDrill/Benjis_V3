a7e020f2bd74bc0c058186a9c000468459b29399
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-28 20:35:57 -0700

    Started styling

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="./css/header_footer.css">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+
+    <title>Benjis | Your ideas to web</title>
+</head>
+<body>
+
+    <nav>
+        <img alt="The red logo of our business"/>
+        <li>
+            <ul>
+            Process
+            </ul>
+
+            <ul>
+            Portfolio
+            </ul>
+
+            <ul>
+            Contact
+            </ul>
+        </li>
+    </nav>
+
+    <header>
+        <img alt="The left side of the header which features an image of some sort"/>
+
+        <div>
+            <h1>We take your ideas and translate them into web designs</h1>
+            <h3></h3>
+            <button>Our Work</button>
+        </div>
+    </header>
+
+    <section id='section-one'>
+        <div id='so-left'>
+            <div id='so-left-header'>
+                <img />
+                <img />
+                <img />
+
+                <div id='so-left-header-txt'>
+                    <p>Below explains our work </p>
+                </div>
+            </div>
+
+            <div id='so-left-content'>
+                <h3>Research Phase One</h3>
+                <div>
+                    <h4>-Market</h4>
+                    <p>The first step we take for web building is designing around your business.<br><br>
+
+We like to research the type of market you excel in. This allows us to determine which components are necessary when designing for your website. <br><br>
+
+This first step is crucial, and ensures you receive a site that fits you best.</p>
+                   
+                    <h4>-SEO</h4>
+                    <p>Proper SEO (search engine optimization) practice is key when building functional websites. <br><br>
+
+We implement industry standard techniques when that vibe well in your market.</p>
+                </div>
+            </div>
+        </div>
+
+        <img id='so-right' alt="The left side of this section features an image"/>
+    </section>
+
+    <section id="section-two">
+        <img />
+
+        <div id='so-two-content'>
+            <h3>Design Phase Two</h3>
+
+            <div id='so-two-txt'>
+                <h4>-Start Designing</h4>
+                <p>After phase one is finished we grab pen and paper and start designing. We take our first phase of research and design the building blocks of the website. All knowledge is well used as this is one of the most crucial parts of this process. </p>
+           
+
+                <h4>-Modern  Techniques</h4>
+                <p>Using modern design program we build UI/UX that is well crafted and suited to your website needs. </p>
+       
+
+                <h4>-Designer</h4>
+                <p>Our on hand graphic designer ensures every design is well crafted and used to ensure great design.</p>
+            </div>
+         </div>
+    </section>
+
+    <section id='section-three'>
+        <div id='so-three-content'>
+            <h3>
+            Build Phase three
+            </h3>
+            <p>
+            This is the fun and final part of our work process. After phase one and two are finished, we combine the research and design to create you a fully custom website!
+            </p>
+        </div>
+
+        <div id='so-three-hero'>
+            <img />
+            <div>
+                <h3>
+                Benjamin Sanchez
+                </h3>
+                <p>
+                Hi, my job is to ensure your website works. I practice web building using pure web languages such as HTML, CSS, and JavaScript. I am currently studying computer science at Columbia Basin College, and I enjoy designing and programming  for friends and family.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div>
+            <div>
+                <h2>
+                <!-- Generate through php -->
+                </h2>
+                <p>
+                <!-- Generate through php -->
+                </p>
+            </div>
+        </div>
+
+        <div>
+            <!-- Generate through PHP? -->
+        </div>
+    </section>
+
+    <section>
+
+        <div>  
+            <h2>
+            Let us help you design your next website
+            </h2>
+            <input type="email" placeholder="example@benjis-web.com">
+            <button>Submit</button> 
+
+        </div>
+    
+    </section>
+
+
+</body>
+</html>
